fd58fd9fb67709467353926814b136e65a4adfe5
Msmukil <79651657+Msmukil@users.noreply.github.com>
2022-05-06 08:56:14 +0530

    Add files via upload

---
+++ src/Routing.js
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import "./components/Education";
+
+const routing = {
+    <Router>
+       <div>
+          <div>
+              <Link to="/Education.js"></Link></div>
+         </div>
+         
+         </Router>
+    
+}
